33f92ea979bc2eed6911744fbb8245ed9766fb50
Марат Ильясов <marat.ilyasov.2277@gmail.com>
2025-02-12 17:24:24 +0300

    Создана задача на c#

---
+++ Pr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CircleAreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус круга: ");
+            string radiusInput = Console.ReadLine();
+
+            if (double.TryParse(radiusInput, out double radius))
+            {
+                if (radius >= 0)
+                {
+                    double area = Math.PI * radius * radius;
+
+                    Console.WriteLine("Площадь круга с радиусом " + radius + " равна: " + area);
+                }
+                else
+                {
+                    Console.WriteLine("Радиус не может быть отрицательным.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
